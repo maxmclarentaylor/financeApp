d730094da10dd939cea7e1dced7d2734292d1d52
Max Taylor <max.mclaren.taylor@outlook.com>
2020-10-06 18:24:08 +0100

    added in styling and dropdown list to bring a component back into the app. This is only the first stage of styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,11 +6,6 @@ function Overview() {
   return (
     <div className="App">
          <ItemContainer/>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
     </div>
   );
 }
--- src/containers/itemContainer.js
+++ src/containers/itemContainer.js
@@ -6,19 +6,24 @@ import {
   } from "react-router-dom";
 import { ItemComponent } from '../components/itemComponent'
 import { addItemToKeys } from '../reducers/ducks/items/individualItemHOC'
-
+import { styles } from '../styles/index.css'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 export const ItemContainer = ( ) => {
     const items = useSelector(state => state.item.itemAllId)
     const dispatch = useDispatch()
+    var arrayOfNames = []
+    console.log(arrayOfNames)
 
     return(
-       <div>
+       <div className="routeDisplay" >
           {items.map((item) => {
+              
             const link = `/${item[0]}`
             if(item[1]){
             return(
-            <div key={item[0]}>
+            <div className="itemRoute" key={item[0]}>
                 <nav>
                     <Link to={link}>{item[0]}</Link>
                 </nav>
@@ -29,17 +34,15 @@ export const ItemContainer = ( ) => {
                 </div>
             </div>
                 )
-            }     
+            }  
+            else{
+                arrayOfNames.push(item[0])
+            }   
         })}
-        {items.map((item, index) => {
-            return(
-                <div key={item + index}>
-                    <div onClick={() => {
-                        dispatch(addItemToKeys([item[0], true]))
-                    }}>Add back in {item}</div>
-                </div>
-            )
-        })}
+        {arrayOfNames.length > 0 &&  <Dropdown options={arrayOfNames} onChange={(value) => {
+           dispatch(addItemToKeys([value.value, true]))
+       }} value={""} placeholder="Select an option" /> }
+     
     </div>
     )
 }
